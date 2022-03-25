b1628c9ece1a7e69081c5e31624c56e026f5fb8d
unknown <eya.nani@sesame.com.tn>
2022-03-25 21:46:37 +0100

    provider

--- diyeri/lib/providers/auth_provider.dart
+++ diyeri/lib/providers/auth_provider.dart
@@ -1,43 +1,31 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/foundation.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/material.dart';
+
 class Auth_provider with ChangeNotifier {
 FirebaseAuth auth = FirebaseAuth.instance;
 get currentUser => auth.currentUser;
-var error = '';
+
+// Sign up
 SignUp(email, password) async {
   try  {
-            UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password); 
+    UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password); 
+    }
+  on FirebaseException catch (e) {
+    print(e);
+  }
 }
-        on FirebaseException catch (e) {
-          if (e.code == "weak-password" || e.code == 'email-already-in-use') {
-        
-            error = e.code.replaceAll('-', ' ');
-          
-         }
-        else {
-          error = e.code.replaceAll('-', ' ');
-          print(error);
-          }
-        }
-}
+//login Method
 Login(email, password) async{
-          try {
-          UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
-          }
-          on FirebaseException catch(e) {
-            
-            if (e.code == 'user-not-found' || e.code == 'wrong password')
-             {
-               error = e.code.replaceAll('-', ' ');
-             }
-            else {
-              error = e.code.replaceAll('-', ' ');
-               print(e.code);
-            }
-          }
+  try {
+    UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+    }
+  on FirebaseException catch(e) {
+    print(e);
+    }
  }
+//signout
 SignOut() async {
   await FirebaseAuth.instance.signOut();
 }
